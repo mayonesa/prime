28e95bcb00fcc6418b731ccceabb9911f2796922
John Jimenez <mayonesa.cosmica@gmail.com>
2018-02-28 13:23:07 -0500

    tab -> spaces

--- test/models/PrimeSpec.scala
+++ test/models/PrimeSpec.scala
@@ -4,20 +4,20 @@ import org.scalatestplus.play._
 import concurrent.ExecutionContext.Implicits.global
 
 class PrimeSpec extends PlaySpec {
-	private val p = new Prime()
+  private val p = new Prime()
 	
-	"Prior primes" must {
-		"for 5 be empty at first" in {
-			p.getPriorPrimes(5) mustBe IndexedSeq.empty[Int]
-		}
-		"for 5 be [2, 3] after the first time" in {
-			p.getPriorPrimes(5) mustBe IndexedSeq(2, 3)
-		} 
-		"for 8 be empty at first" in {
-			p.getPriorPrimes(8) mustBe IndexedSeq.empty[Int]
-		}
-		"for 8 be [2, 3, 5] after the first time" in {
-			p.getPriorPrimes(8) mustBe IndexedSeq(2, 3, 5, 7)
-		} 
-	}
+  "Prior primes" must {
+    "for 5 be empty at first" in {
+      p.getPriorPrimes(5) mustBe IndexedSeq.empty[Int]
+    }
+    "for 5 be [2, 3] after the first time" in {
+      p.getPriorPrimes(5) mustBe IndexedSeq(2, 3)
+    } 
+    "for 8 be empty at first" in {
+      p.getPriorPrimes(8) mustBe IndexedSeq.empty[Int]
+    }
+    "for 8 be [2, 3, 5] after the first time" in {
+      p.getPriorPrimes(8) mustBe IndexedSeq(2, 3, 5, 7)
+    } 
+  }
 }
