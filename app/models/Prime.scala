e43e99f5206a1cdde5cdd2e6fcb1282951ac1e35
John Jimenez <mayonesa.cosmica@gmail.com>
2018-02-28 02:29:08 -0500

    tabs -> spaces

--- app/models/Prime.scala
+++ app/models/Prime.scala
@@ -6,19 +6,19 @@ import org.apache.commons.math3.primes.Primes
 import common.PrimesMap
 
 class Prime @Inject() (implicit ec: ExecutionContext) {
-	private val priorPrimesMap = PrimesMap.empty
+  private val priorPrimesMap = PrimesMap.empty
 	
-	def isPrime(n: Int): Future[Boolean] = Future(Primes.isPrime(n))
+  def isPrime(n: Int): Future[Boolean] = Future(Primes.isPrime(n))
 		
-	def getPriorPrimes(n: Int): Future[Seq[Int]] = Future(priorPrimesMap.synchronized {
-	  if (priorPrimesMap.contains(n)) priorPrimesMap(n)
-		else {
-			Future(updatePriorPrimes(n))
-			Seq.empty[Int]
-		}
-	})
+  def getPriorPrimes(n: Int): Future[Seq[Int]] = Future(priorPrimesMap.synchronized {
+    if (priorPrimesMap.contains(n)) priorPrimesMap(n)
+    else {
+      Future(updatePriorPrimes(n))
+      Seq.empty[Int]
+    }
+  })
 	
-	private def updatePriorPrimes(n: Int) = priorPrimesMap(n) = priorPrimes(n)
+  private def updatePriorPrimes(n: Int) = priorPrimesMap(n) = priorPrimes(n)
 	
-	private def priorPrimes(n: Int) = (2 until n).filter(Primes.isPrime(_))
+  private def priorPrimes(n: Int) = (2 until n).filter(Primes.isPrime(_))
 }
