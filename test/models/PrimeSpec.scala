4088f5391e3c1cd23031ed0ebf2caa6f26552fc4
John Jimenez <mayonesa.cosmica@gmail.com>
2018-02-28 15:02:35 -0500

    proper result message. cleaner mathcher

--- test/models/PrimeSpec.scala
+++ test/models/PrimeSpec.scala
@@ -8,15 +8,15 @@ class PrimeSpec extends PlaySpec {
 	
   "Prior primes" must {
     "for 5 be empty at first" in {
-      p.getPriorPrimes(5) mustBe IndexedSeq.empty[Int]
+      p.getPriorPrimes(5) mustBe empty
     }
     "for 5 be [2, 3] after the first time" in {
       p.getPriorPrimes(5) mustBe IndexedSeq(2, 3)
     } 
     "for 8 be empty at first" in {
-      p.getPriorPrimes(8) mustBe IndexedSeq.empty[Int]
+      p.getPriorPrimes(8) mustBe empty
     }
-    "for 8 be [2, 3, 5] after the first time" in {
+    "for 8 be [2, 3, 5, 7] after the first time" in {
       p.getPriorPrimes(8) mustBe IndexedSeq(2, 3, 5, 7)
     } 
   }
