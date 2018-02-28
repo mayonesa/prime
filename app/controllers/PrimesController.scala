85867e1d5a8349ca983f430221f9e073de256071
John Jimenez <mayonesa.cosmica@gmail.com>
2018-02-28 02:31:11 -0500

    tabs -> spaces

--- app/controllers/PrimesController.scala
+++ app/controllers/PrimesController.scala
@@ -8,11 +8,11 @@ import play.api.libs.json.Json
 
 class PrimesController @Inject() (cc: ControllerComponents,
 																	prime: Prime)(implicit ec: ExecutionContext) extends AbstractController(cc) {
-	def primes(n: Int) = Action.async { 
-		val priorPrimesFut = prime.getPriorPrimes(n)
-		for {
-			isPrime <- prime.isPrime(n)
-			priorPrimes <- priorPrimesFut
-		} yield	Ok(Json.obj("isPrime" -> isPrime, "primes" -> Json.toJson(priorPrimes)))
-	}
+  def primes(n: Int) = Action.async { 
+    val priorPrimesFut = prime.getPriorPrimes(n)
+    for {
+      isPrime <- prime.isPrime(n)
+      priorPrimes <- priorPrimesFut
+    } yield	Ok(Json.obj("isPrime" -> isPrime, "primes" -> Json.toJson(priorPrimes)))
+  }
 }
