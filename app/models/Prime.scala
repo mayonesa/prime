065a79008c48f89f683dce1d5e8463d3aa650fd1
John Jimenez <mayonesa.cosmica@gmail.com>
2018-03-01 19:22:32 -0500

    simpler execution context

--- app/models/Prime.scala
+++ app/models/Prime.scala
@@ -3,12 +3,12 @@ package models
 import concurrent.{ ExecutionContext, Future }
 import javax.inject.Inject
 import org.apache.commons.math3.primes.Primes
-import java.util.concurrent.Executors.newFixedThreadPool
+import java.util.concurrent.Executors.newSingleThreadExecutor
 
 object Prime {
   private val lock = AnyRef
   private val priorPrimes = PriorPrimes(lock)
-  implicit private val ec = ExecutionContext.fromExecutor(newFixedThreadPool(1))
+  implicit private val ec = ExecutionContext.fromExecutor(newSingleThreadExecutor)
 	
   def isPrime(n: Int): Boolean = Primes.isPrime(n)
 		
